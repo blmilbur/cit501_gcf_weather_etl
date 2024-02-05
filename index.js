d0bec7dbb479a68c56d7ef3c011b18e6bb5e7e48
Blaine Milburn <blmilbur@iu.edu>
2024-02-05 20:30:01 +0000

    End of Version 1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const csv = require("csv-parser")
+
 exports.readObservation = (file, context) => {
     console.log(`  Event: ${context.eventId}`);
     console.log(`  Event Type: ${context.eventType}`);
     console.log(`  Bucket: ${file.bucket}`);
     console.log(`  File: ${file.name}`);
+    console.log`${file.createReadStream()}`
 }
